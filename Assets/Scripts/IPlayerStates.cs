491a467e8be8b64e3db03c44d697fa75a30f406f
hyugowacha <uena0506@gmail.com>
2025-04-18 18:30:31 +0900

    [Feat] 플레이어 컨트롤러 작성중

---
+++ Assets/Scripts/IPlayerStates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerState
+{
+    void EnterState(PlayerController player);
+    void UpdateState(PlayerController player);
+    void FixedUpdateState(PlayerController player);
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 dir;
+    Vector3 moveDir;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMove(InputAction.CallbackContext ctx)
+    {
+        dir = ctx.ReadValue<Vector2>();
+        moveDir = new Vector3(dir.x, 0 ,dir.y);
     }
 }
