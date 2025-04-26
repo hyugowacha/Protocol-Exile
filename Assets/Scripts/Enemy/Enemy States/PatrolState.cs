02303795ef74e8029b9a1971bfa8f1c75723b748
hyugowacha <uena0506@gmail.com>
2025-04-26 17:32:02 +0900

    [feat] 적 배회 AI

--- Assets/Scripts/Enemy/Enemy States/PatrolState.cs
+++ Assets/Scripts/Enemy/Enemy States/PatrolState.cs
@@ -7,25 +7,57 @@ public class PatrolState : IEnemyState
     public Transform positionA;
     public Transform positionB;
     EnemyAController enemyA;
+    float nowSpeed;
 
     float waitCoolTime = 4.0f;
-    float waitElapsedTime;
+
+    int pointNum = 0;
 
     public void EnterState(EnemyBase enemy)
     {
         enemyA = (EnemyAController)enemy;
         enemyA.nowState = SoldierState.Patrol;
+        enemyA.agent.stoppingDistance = 0.5f;
 
-        enemyA.agent.SetDestination(enemyA.wayPoints[0].position);
+        enemyA.agent.SetDestination(enemyA.wayPoints[pointNum].position);
+
+        enemy.StartCoroutine(Patrol());
     }
 
     public void UpdateState(EnemyBase enemy)
     {
-
+        nowSpeed = enemyA.agent.velocity.magnitude;
+        enemyA.enemyAnimator.SetFloat("IsMove",nowSpeed);
     }
 
     public void FixedUpdateState(EnemyBase enemy)
     {
+        
+    }
 
+    IEnumerator Patrol()
+    {
+        while(true)
+        {
+            if (enemyA.isPlayerFind)
+            {
+                yield break;
+            }
+
+            if (!enemyA.agent.pathPending && enemyA.agent.remainingDistance < enemyA.agent.stoppingDistance &&
+                enemyA.agent.velocity.sqrMagnitude < 0.01f)
+            {
+                enemyA.agent.isStopped = true;
+                enemyA.enemyAnimator.SetFloat("IsMove", 0f);
+
+                yield return new WaitForSeconds(waitCoolTime);
+
+                pointNum = (pointNum + 1) % enemyA.wayPoints.Length;
+                enemyA.agent.SetDestination(enemyA.wayPoints[pointNum].position);
+                enemyA.agent.isStopped = false;      
+            }
+
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -8,6 +8,13 @@ public class EnemyAController : EnemyBase
     private IEnemyState enemyCurrentState;
     public NavMeshAgent agent;
     public Transform[] wayPoints;
+    public Rigidbody enemyRigid;
+    public Animator enemyAnimator;
+
+    public Collider[] findColliders;
+    public Collider[] attackColliders;
+
+    public bool isPlayerFind;
 
     public SoldierState nowState;
 
@@ -17,6 +24,7 @@ public class EnemyAController : EnemyBase
         moveSpeed = enemyData.moveSpeed;
         sightRange = enemyData.sightRange;
         attackRange = enemyData.attackRange;
+        isPlayerFind = false;
 
         ChangeState(new PatrolState());
     }
@@ -40,6 +48,24 @@ public class EnemyAController : EnemyBase
     void Update()
     {
         enemyCurrentState.UpdateState(this);
+        findColliders = Physics.OverlapSphere(transform.position, sightRange);
+
+        attackColliders = Physics.OverlapSphere(transform.position, attackRange);
+
+        foreach(var hit in findColliders)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                agent.isStopped = true;
+
+                Debug.Log("적 발견");
+                foreach(var hit2 in attackColliders)
+                {
+                    Debug.Log("공격 개시");
+                }
+                
+            }
+        }
     }
 
     private void FixedUpdate()
